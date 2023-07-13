14b683a8a241cceb9849409a5abaf28fc00ca400
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-07-12 21:05:30 -0300

    att trabalho

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import { MainContainer } from "../style"
+
+function ErrorPage() {
+  return (
+    <MainContainer>
+      <Header />
+      <h1>Error Page</h1>
+    </MainContainer>
+  )
+}
+
+export default ErrorPage
